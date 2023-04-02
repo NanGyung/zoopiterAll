816fe085025febe6dbc6b4a62448c130395aad46
NanGyung <rhrnakaktdidrod0926@gmail.com>
2023-04-03 06:39:55 +0900

    MemberDAOImple 수정

--- src/main/java/com/project/zoopiter/domain/member/dao/MemberDAOImpl.java
+++ src/main/java/com/project/zoopiter/domain/member/dao/MemberDAOImpl.java
@@ -7,6 +7,8 @@ import org.springframework.jdbc.core.namedparam.BeanPropertySqlParameterSource;
 import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 import org.springframework.jdbc.core.namedparam.SqlParameterSource;
+import org.springframework.jdbc.support.GeneratedKeyHolder;
+import org.springframework.jdbc.support.KeyHolder;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
